5dc19b783df902e44870bc0988c94f89eed1d630
oriolfornos <oriol-1-2@hotmail.com>
2021-03-24 23:48:47 +0100

    cosas

---
+++ src/Styles.js
@@ -0,0 +1,14 @@
+var styles = {
+    sidePanel: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        padding: "10px",
+        marginTop: "10px"
+    },
+    roomItem: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-start"
+    }
+}
